11eba797d6961bac6560a267d07ae65c0eda47cc
Luisa Larrieu <137114227+Larrieu16@users.noreply.github.com>
2025-06-13 18:12:07 -0300

    remove comments

--- notification-service/src/main/java/com/coelho/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/coelho/notification_service/NotificationServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.coelho.notification_service.repository") // Garanta que o caminho está correto
+@EnableJpaRepositories("com.coelho.notification_service.repository") 
 @EntityScan("com.coelho.notification_service.model")
 public class NotificationServiceApplication {
 	public static void main(String[] args) {
